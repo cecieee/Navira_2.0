31325456e1da66b2a4b32ee28bb4bea42ad308c2
Glitch-hash01 <harikichus2018@gmail.com>
2025-12-01 22:00:29 +0530

    feat: add Heading,Glimpse

--- src/sections/Glimpse.jsx
+++ src/sections/Glimpse.jsx
@@ -1,17 +1,23 @@
-import ImageSlider from "../components/ImageSlider";
+
+import { Marquee } from "../components/ui/marquee";
+import Heading from "../components/Heading";
+
 
 export default function Glimpse() {
   return (
     <div className="flex flex-col gap-10 items-center justify-center">
-      <h1 className="font-primary text-white">Glimpse Navira</h1>
-      <div className="flex flex-col gap-10">
-        <ImageSlider pauseOnHover>
-          <img
-            src="/glimpse1.jpg"
-            alt="Glimpse 1"
-            className="h-48 w-auto object-cover rounded-lg"
-          />
-        </ImageSlider>
+      <Heading mainTitle="GLIMPSE" highlightedTitle="NAVIRA" />
+      <div className="flex flex-col ">
+        <Marquee>
+          <img src="/assets/images/navira.jpeg"  alt="" className="w-100 rounded-xl"/>
+          <img src="/assets/images/navira2.jpeg" alt="" className="w-100 rounded-xl"/>
+          <img src="/assets/images/navira2.jpeg" alt="" className="w-100 rounded-xl"/>
+        </Marquee>
+        <Marquee reverse={true}>
+          <img src="/assets/images/navira.jpeg"  alt="" className="w-100 rounded-xl"/>
+          <img src="/assets/images/navira2.jpeg" alt="" className="w-100 rounded-xl"/>
+          <img src="/assets/images/navira2.jpeg" alt="" className="w-100 rounded-xl"/>
+        </Marquee>
       </div>
     </div>
   );
